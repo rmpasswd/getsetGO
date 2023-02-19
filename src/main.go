6744801577225e73a77b8d87fa6605e3f137226f
Mahin Ahmad <wxyzabc190@gmail.com>
2023-02-19 20:17:45 +0600

    First commit.
    playing w/ variables.

---
+++ src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+//	var name string = "Mahin"
+	var letter2u rune
+	letter2u = 'Q'
+//	var age int = 22
+	var IsFound bool
+	fmt.Println(IsFound, letter2u )
+
+var chr rune
+chr = 'র'
+fmt.Println(chr)
+fmt.Printf("%c", chr)
+}
---
+++ src/variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*var name string = "AhmadMahin"
+	var name, city string = "MA", "DHA"
+	fmt.Println(name,city) */
+}
+
